b7c96a873f76488fd9db68c2bf86c82b4bc9f75f
Kennie Jay Davis <kenniejaydavis@gmail.com>
2013-05-28 06:59:37 -0700

    added comments for readability

--- FNE.Eventing/EventBroker.cs
+++ FNE.Eventing/EventBroker.cs
@@ -7,18 +7,36 @@ using Microsoft.AspNet.SignalR;
 
 namespace FNE.Eventing
 {
+    /// <summary>
+    /// Adds client handlers for, removes client handlers from and dispatches named event messages.
+    /// </summary>
     public class EventBroker : Hub
     {
+        /// <summary>
+        /// Adds a client handler for the named event.
+        /// </summary>
+        /// <param name="event">The name of the event.</param>
+        /// <returns></returns>
         public Task AddHandler(string @event)
         {
             return Groups.Add(Context.ConnectionId, @event);
         }
 
+        /// <summary>
+        /// Removes a client handler from the named event.
+        /// </summary>
+        /// <param name="event">The name of the event.</param>
+        /// <returns></returns>
         public Task RemoveHandler(string @event)
         {
             return Groups.Remove(Context.ConnectionId, @event);
         }
 
+        /// <summary>
+        /// Dispatches an event messages to all clients of the named event.
+        /// </summary>
+        /// <param name="event">The name of the event.</param>
+        /// <param name="message">The event message.</param>
         public void Dispatch(string @event, dynamic message)
         {
             Clients.OthersInGroup(@event).Dispatch(@event, message);
