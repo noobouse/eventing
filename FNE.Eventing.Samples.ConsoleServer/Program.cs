d92f4279e27b7dd05f1d12a89e074125ed4a0d04
Kennie Jay Davis <kenniejaydavis@gmail.com>
2013-05-28 07:27:52 -0700

    added sample server project

---
+++ FNE.Eventing.Samples.ConsoleServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eventing = FNE.Eventing;
+
+namespace FNE.Eventing.Samples.ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Eventing.Server.Start(@"http://localhost:8088"))
+            {
+                Console.WriteLine("EventBroker server started...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
