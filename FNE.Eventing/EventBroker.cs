8bcc6979e14c04a734bd318c7147a78bd51bc09d
Kennie Jay Davis <kenniejaydavis@gmail.com>
2013-05-28 22:28:45 -0700

    added logging to event broker hub

--- FNE.Eventing/EventBroker.cs
+++ FNE.Eventing/EventBroker.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FNE.Eventing.Logging;
 using Microsoft.AspNet.SignalR;
 
 namespace FNE.Eventing
@@ -12,6 +13,33 @@ namespace FNE.Eventing
     /// </summary>
     public class EventBroker : Hub
     {
+        //
+        // Fields
+
+        private ILoggingProvider loggingProvider;
+
+        //
+        // Constructors
+
+        #region EventBroker
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventBroker" class./>
+        /// </summary>
+        /// <param name="loggingProvider">The <see cref="ILoggingProvider"/> instance to use.</param>
+        public EventBroker(ILoggingProvider loggingProvider = null)
+            : base()
+        {
+            this.loggingProvider = loggingProvider;
+        }
+
+        #endregion
+
+        //
+        // Methods
+
+        #region AddHandler
+        
         /// <summary>
         /// Adds a client handler for the named event.
         /// </summary>
@@ -22,6 +50,10 @@ namespace FNE.Eventing
             return Groups.Add(Context.ConnectionId, @event);
         }
 
+        #endregion
+
+        #region RemoveHandler
+        
         /// <summary>
         /// Removes a client handler from the named event.
         /// </summary>
@@ -32,6 +64,10 @@ namespace FNE.Eventing
             return Groups.Remove(Context.ConnectionId, @event);
         }
 
+        #endregion
+
+        #region Dispatch
+        
         /// <summary>
         /// Dispatches an event messages to all clients of the named event.
         /// </summary>
@@ -41,5 +77,64 @@ namespace FNE.Eventing
         {
             Clients.OthersInGroup(@event).Dispatch(@event, message);
         }
+
+        #endregion
+
+        //
+        // Hub Overrides
+
+        #region OnConnected
+        
+        public override Task OnConnected()
+        {
+            try
+            {
+                return base.OnConnected();
+            }
+
+            finally
+            {
+                if (this.loggingProvider != null)
+                    this.loggingProvider.LogInformation("Client {{{0}}} connected at {1}", Context.ConnectionId, DateTime.Now);
+            }
+        }
+
+        #endregion
+
+        #region OnDisconnected
+        
+        public override Task OnDisconnected()
+        {
+            try
+            {
+                return base.OnDisconnected();
+            }
+
+            finally
+            {
+                if (this.loggingProvider != null)
+                    this.loggingProvider.LogInformation("Client {{{0}}} disconnected at {1}", Context.ConnectionId, DateTime.Now);
+            }
+        }
+
+        #endregion
+
+        #region OnReconnected
+        
+        public override Task OnReconnected()
+        {
+            try
+            {
+                return base.OnReconnected();
+            }
+
+            finally
+            {
+                if (this.loggingProvider != null)
+                    this.loggingProvider.LogInformation("Client {{{0}}} reconnected at {1}", Context.ConnectionId, DateTime.Now);
+            }
+        }
+
+        #endregion
     }
 }
