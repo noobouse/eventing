d7c2212ce5130cefc1a1fcf4145637f4bfc24b69
Kennie Jay Davis <kenniejaydavis@gmail.com>
2013-05-28 22:18:50 -0700

    enumeration to specify log message severity

---
+++ FNE.Eventing/Logging/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FNE.Eventing.Logging
+{
+    /// <summary>
+    /// Specifies the severity of a log message.
+    /// </summary>
+    public enum Severity
+    {
+        /// <summary>
+        /// Indicates an informative message.
+        /// </summary>
+        Information = 0,
+
+        /// <summary>
+        /// Indicates a potential problem, but not a fatal processing error.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Indicates a fatal processing error.
+        /// </summary>
+        Error
+    }
+}
