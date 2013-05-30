a38ddaadf27b688506271fa429d52ac3f2a39c24
Kennie Jay Davis <kenniejaydavis@gmail.com>
2013-05-30 14:11:50 -0700

    moved logging namespace to new assembly

---
+++ FNE.Eventing.Logging/ILoggingProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNE.Eventing.Logging
+{
+    /// <summary>
+    /// Provides methods to log information, warnings and errors during event processing.
+    /// </summary>
+    public interface ILoggingProvider
+    {
+        /// <summary>
+        /// Logs a message.
+        /// </summary>
+        /// <param name="severity">The <see cref="LCC.Eventing.Logging.Severity"/> of the message.</param>
+        /// <param name="message">A composite format log message.</param>
+        /// <param name="args">An object array that contains zero or more objects to format.</param>
+        void Log(Severity severity, string message, params object[] args);
+    }
+}
